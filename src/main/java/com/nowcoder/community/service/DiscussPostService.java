7631fb5e49374b5813a2997ebdad1dd426191a57
cYLoVeMm <910986085@qq.com>
2022-06-27 15:44:46 +0800

    第二次提交

--- src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -8,16 +8,21 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+/**
+ * 帖子业务
+ */
 @Service
 public class DiscussPostService {
 
     @Autowired
     private DiscussPostMapper discussPostMapper;
 
+    //帖子列表
     public List<DiscussPost> findDiscussPosts(int userId, int offset,int limit){
         return discussPostMapper.selectDiscussPosts(userId, offset, limit);
     }
 
+    //帖子数量
     public int findDiscussPostRows(int userId){
         return discussPostMapper.selectDiscussPostRows(userId);
     }
---
+++ src/test/java/com/nowcoder/community/MailTests.java
@@ -0,0 +1,49 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.dao.AlphaDao;
+import com.nowcoder.community.service.AlphaService;
+import com.nowcoder.community.util.MailClient;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class MailTests {
+
+    @Autowired
+    private MailClient mailClient;
+
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void testTextMail(){
+        mailClient.sendMail("2392135045@qq.com", "方小梅小傻瓜","喜欢臭臭的小梅");
+    }
+
+    @Test
+    public void testHtmlMail(){
+        Context context = new Context();
+        context.setVariable("username", "sunday");
+
+        String content = templateEngine.process("/mail/demo", context);
+
+        System.out.println(content);
+
+        mailClient.sendMail("256882828@qq.com", "HTML", content);
+    }
+}
